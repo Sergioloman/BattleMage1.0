ce22babf8f479f0772269d17725a310fad1d7bff
Sergio Torres <sergio.lo.torres@gmail.com>
2021-03-07 02:49:37 -0600

    making the document DRY

--- JavaScrip.js
+++ JavaScrip.js
@@ -95,30 +95,3 @@ function fight() {
 
 };
 
-//     Enemies.health[i]  = Enemies.health[i] - playerHero.attack;
-//     console.log( Enemies.name[i] + " still has " + Enemies.health[i] + " life. ");
-//     playerHero.health = playerHero - Enemies.attack[i];
-//     console.log( playerHero.name + " has " + playerHero.health + " life! ");
-
-//     if (Enemies.health <= 0){
-//         window.alert(Enemies.name[i] + ' has died x.x ');
-//         playerHero.level = playerHero.level + Enemies.level[i];
-
-//     } else {
-//         window.alert(Enemies.name[i] + ' still has ' + Enemies.health[i] + ' health left... ');
-//     };
-
-
-// while (playerName.health > 0 && Enemies.health > 0){
-//     var readyPrompt = window.prompt(" type 'Fight' or 'Skip' below")
-//         if (promptFight === "SKIP" || promptFight === "skip" ){
-//             var confirmSkip = window.confirm('are you sure you want to quit?');
-//             if (confirmSkip){
-//                 window.alert(playerName + ' has left, shame on you');
-//                 playerName.level = level - 1;
-//                 console.log(playerName.level);
-//                 break;
-//             }
-//         }
-//     };
-// }
